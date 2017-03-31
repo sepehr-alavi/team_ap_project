4fa5880d8f1f1199afdd3bbb4af34284ddc3d014
Sepehr <sepehr_al@yahoo.com>
2017-03-31 18:05:48 +0430

    test

---
+++ IdentifiableObject.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Sepehr on 3/31/2017.
+ */
+public class IdentifiableObject {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
