921000139021219debecce045621ad61e28679a2
Sepehr <sepehr_al@yahoo.com>
2017-03-31 22:31:12 +0430

    Added LandingStrip class

--- LandingStrip.java
+++ LandingStrip.java
@@ -1,24 +1,15 @@
 import com.sun.deploy.net.cookie.DeployCookieSelector;
+import com.sun.org.apache.bcel.internal.generic.LAND;
 
 public class LandingStrip extends IdentifiableObject{
     private Coordinate begginingTreshold;
     private Coordinate endTreshold;
 
-    public Coordinate getBegginingTreshold() {
-        return begginingTreshold;
-    }
-
-    public void setBegginingTreshold( double lat, double lon ) {
-        this.begginingTreshold.setLat( lat );
-        this.begginingTreshold.setLon( lon );
-    }
-
-    public Coordinate getEndTreshold() {
-        return endTreshold;
-    }
-
-    public void setEndTreshold( double lat, double lon ) {
-        this.begginingTreshold.setLat( lat );
-        this.begginingTreshold.setLon( lon );
+    //Constructor
+    public LandingStrip( String name, double begginingTresholdLat, double begginingTresholdLon, double endTresholdLat, double endTresholdLon ){
+        this.begginingTreshold.setLat( begginingTresholdLat );
+        this.begginingTreshold.setLon( begginingTresholdLon );
+        this.endTreshold.setLat( endTresholdLat );
+        this.endTreshold.setLon( endTresholdLon );
     }
 }
